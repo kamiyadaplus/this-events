7bd29a74e0f89227d20af005742e5b5888491ac4
yung kahuna <tjfaustino7@gmail.com>
2019-01-29 21:46:10 -1000

    two

--- js/app.js
+++ js/app.js
@@ -19,8 +19,21 @@ function showHideNavMenuItem(){
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
-var showSecItem = document.getElementsByClassName('name');
+var secItem = document.getElementsByClassName('name');
 //console.log(showSecItem);
+
+for(var i=0; i<secItem.length; i++){
+    secItem[i].addEventListener('click', shSecItem);
+}
+
+function shSecItem(){
+    var childSecItem = this.querySelectorAll('.menu')[0];
+    if(childSecItem.style.display === 'block'){
+        childSecItem.style.display = 'none';
+    }else{
+        childSecItem.style.display = 'block'
+    }
+}
 
 
 /*3) to the thumbs down icon that will add a count (counter) for each time the icon is clicked on.*/

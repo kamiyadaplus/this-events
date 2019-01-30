a54e29add83355f0b16a6f24771a5afb76a1efbc
yung kahuna <tjfaustino7@gmail.com>
2019-01-29 21:39:38 -1000

    one

--- js/app.js
+++ js/app.js
@@ -2,9 +2,25 @@
 //Add event listeners to the following:
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
+var navElem = document.getElementsByClassName('navi');
+//console.log(navElem);
 
+for(var i=0; i<navElem.length; i++){
+    navElem[i].addEventListener('click', showHideNavMenuItem);
+}
+
+function showHideNavMenuItem(){
+    var childShowHide = this.querySelectorAll('.inner')[0];
+    if(childShowHide.style.display === 'block'){
+        childShowHide.style.display = 'none';
+    }else{
+        childShowHide.style.display = 'block';    }
+}
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+
+var showSecItem = document.getElementsByClassName('name');
+//console.log(showSecItem);
 
 
 /*3) to the thumbs down icon that will add a count (counter) for each time the icon is clicked on.*/
